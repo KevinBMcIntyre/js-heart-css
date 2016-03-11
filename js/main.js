918796803db4086b176b7a667bed97b1b79d8085
Kevin McIntyre <kevin@Kevins-MacBook-Pro.local>
2016-03-11 09:15:24 -0500

    started working on jquery

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var $header = $('header');
+var $h1 = $('h1');
+var $main = $('main');
+var $p1 = $('main p:first-child');
+	$header.addClass('masthead');
+	$h1.removeClass('big-heading').addClass('logo');
+	$main.addClass('wrapper');
+	$p1.addClass('intro');
